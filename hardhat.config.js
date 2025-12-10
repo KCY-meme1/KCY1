d29d717d291c5f845d30db85e28e89c62239d30a
gdbobfasul <gdbop@abv.bg>
2025-12-10 12:27:05 +0600

    KCY-meme-1 171 passing, fixing hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,12 +18,12 @@ module.exports = {
     bscTestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     },
     bscMainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   },
   etherscan: {
